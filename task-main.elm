4f843ef6ec1a1739cb5366f0cedd46a7b7d6b5e8
Adrian <adicirstei@gmail.com>
2016-01-19 14:39:38 +0200

    initial commit

---
+++ task-main.elm
@@ -0,0 +1,31 @@
+module Main where
+import Html exposing (..)
+import Html.Events as E
+
+import Signal
+
+type alias Model = Int
+
+type Action
+  = Idle
+  | History
+  | Increment
+
+actMailbox = Signal.mailbox Idle
+initialModel = 77
+
+update : Action -> Model -> Model
+update a m =
+  m + 1
+
+view : Signal.Address Action -> Model -> Html
+view a m =
+  div []
+    [ h1 [] [ text "Buuuu! ", text <| toString m ]
+    , button [ E.onClick a Increment ] [ text "+" ]
+  ]
+
+
+modelStream = Signal.foldp (update) initialModel actMailbox.signal
+
+main = Signal.map (view actMailbox.address) modelStream
